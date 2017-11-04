541919c04585e93579fe253d32901e423b1b9fff
Alexes Presswood <ampress727@gmail.com>
2017-11-04 13:07:24 -0500

    Add Graphql

--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -17,3 +17,17 @@ export default function Template({
     </div>
   );
 }
+
+export const pageQuery = graphql`
+  query BlogPostByPath($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        path
+        title
+      }
+    }
+  }
+`
+;
