052ab09e6ece1fb116f798afe92bb68969f32149
Alexes Presswood <ampress727@gmail.com>
2017-11-04 12:57:52 -0500

    Add Plugins

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,6 +1,23 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Your Name - Blog`,
+    author: `Your Name`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: []
+      }
+    },
+  ]
 }
