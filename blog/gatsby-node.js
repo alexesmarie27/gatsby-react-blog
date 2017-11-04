a871f15782b2846922536b3fdf3c9eebdc805297
Alexes Presswood <ampress727@gmail.com>
2017-11-04 13:15:53 -0500

    Create Pages

--- blog/gatsby-node.js
+++ blog/gatsby-node.js
@@ -29,5 +29,14 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
         if (result.errors) {
         return Promise.reject(result.errors);
         }
+
+        result.data.allMarkdownRemark.edges
+        .forEach(({ node }) => {
+          createPage({
+            path: node.frontmatter.path,
+            component: blogPostTemplate,
+            context: {}
+          });
+        });
     });
 }
